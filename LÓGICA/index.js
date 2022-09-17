4c25dd2a0237aeb4da97d98ec69ebd490cd5b881
GABRIELITO7 <itodidow12@gmail.com>
2022-09-17 19:37:09 -0300

    Cursos Lógica

---
+++ LÓGICA/index.js
@@ -0,0 +1,30 @@
+console.log("GABRIEL")
+console.log(7)
+console.log(false)
+console.log(true) 
+
+// sinal que representa E : &&
+// sinal que representa OU: ||
+
+var resultado = false && true
+
+//sinais de comparação : > , < , =, <=, >=
+
+//sinais matematicos : + , - , *, / , ^
+
+var idadeGabriel = 19
+
+var maiorDeIdade = 18
+
+var nome = "Gabriel"
+
+var idadeAposentar = 65
+
+var ehMaiorDeIdade = (idadeGabriel >= maiorDeIdade) && (idadeGabriel >= idadeAposentar);
+
+console.log(ehMaiorDeIdade);
+
+console.log("A idade de " + nome + " é " + idadeGabriel);
+
+var Soma = idadeGabriel - maiorDeIdade 
+console.log(Soma) 
